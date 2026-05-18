16f088489a4c8ab46f13d968728ba47c2bd15e4b
David R. <neisserrey@upeu.edu.pe>
2026-05-18 01:31:43 -0500

    fix: :sparkles: Corrige migración y modelo para Empleado

--- app/models/empleado.py
+++ app/models/empleado.py
@@ -9,6 +9,8 @@ class Empleado(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     tipo_documento: str = Field(max_length=10)
     numero_documento: str = Field(index=True, max_length=20)
+    apellidos: str
+    nombres: str
     nombre_completo: str
     profesion: str
     numero_de_colegiatura: str = Field(max_length=20)
